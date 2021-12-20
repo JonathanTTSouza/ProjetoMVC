acfde6cbc955b36014f380b3096409ef6fa6942f
JonathanTTSouza <68395424+JonathanTTSouza@users.noreply.github.com>
2021-12-20 09:29:36 -0300

    First Model-Controller-View - Department

--- ProjetoMVC/ProjetoMVC/Controllers/HomeController.cs
+++ ProjetoMVC/ProjetoMVC/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using ProjetoMVC.Models;
+using ProjetoMVC.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
